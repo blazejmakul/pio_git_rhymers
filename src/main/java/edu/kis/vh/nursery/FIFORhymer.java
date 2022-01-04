2ff72a7f387c26ec3dc0ec381f706fec5dc8f388
Błazej <blazemakul@gmail.com>
2022-01-04 22:42:51 +0100

    1.5.6 - dodano javadoc do FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/*
+ * FIFORhymer is a rhymer extending DefaultCountingOutRhymer
+ * Override countOut method, rest of methods are the same as in default
+ *
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
